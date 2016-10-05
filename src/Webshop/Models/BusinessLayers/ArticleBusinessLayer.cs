de300ebbbcc757fcf47728c27299ebefac8269bf
Andreas Bergh <andreas__bergh@hotmail.com>
2016-10-05 23:10:04 +0200

    Refactoring businesslayer

--- src/Webshop/Models/BusinessLayers/ArticleBusinessLayer.cs
+++ src/Webshop/Models/BusinessLayers/ArticleBusinessLayer.cs
@@ -15,7 +15,7 @@ namespace Webshop.Models.BusinessLayers
 {
     public class ArticleBusinessLayer
     {
-        private static readonly string[] _imageFileExtensions = { ".jpg", ".png", ".gif", ".jpeg" };
+        private static readonly string[] _imageFileExtensions = { ".jpg", ".png", ".gif", ".jpeg" }; // for image checking
         private readonly WebShopRepository _context;
         private readonly IHostingEnvironment _hostEnvironment;
         private readonly IStringLocalizer<ArticleController> _localizer;
@@ -52,27 +52,17 @@ namespace Webshop.Models.BusinessLayers
             var rootHost = RootHost(hostEnvironment);
 
             artTranslate.LangCode = "sv";
-
-            _vendorID = VendorID;
-            _vendor = context.Vendors.Where(x => x.VendorID == _vendorID).Select(n => n.VendorName).FirstOrDefault();
-            article.VendorId = Convert.ToInt32(_vendorID);
-
-            _categoryID = CategoryID;
-            _category = context.Categories.Where(x => x.CategoryID == _categoryID).Select(n => n.CategoryName).FirstOrDefault();
-            article.CategoryId = _categoryID;
-
-            _productID = ProductID;
-            _product = context.Products.Where(x => x.ProductID == _productID).Select(n => n.ProductName).FirstOrDefault();
-            article.ProductId = _productID;
-
-            _subproductID = SubCategoryID;
-            _subproduct = context.SubCategories.Where(x => x.SubCategoryID == _subproductID).Select(n => n.SubCategoryName).FirstOrDefault();
-            article.SubCategoryId = _subproductID;
-
-            tempArtNr = String.Format("{0}{1}{2}{3}", _vendorID, _categoryID, _productID, _subproductID);
-            article.ArticleNumber = tempArtNr;
-            artTranslate.ArticleNumber = tempArtNr;
-
+            // get vendor
+            GetVendor(context, article, VendorID);
+            // get category
+            GetCategory(context, article, CategoryID);
+            // get product
+            GetProduct(context, article, ProductID);
+            // get subcategory
+            GetSubCategory(context, article, SubCategoryID);
+            // set tempfilename for image upload
+            SetTemFileName(article, artTranslate);
+            // creates a gui for image<>article relation
             Guid guidID = CreatGuid();
             article.ArticleGuid = guidID;
             article.ArticleAddDate = date;
@@ -87,7 +77,42 @@ namespace Webshop.Models.BusinessLayers
             context.ArticleTranslations.Add(artTranslate);
             await context.SaveChangesAsync();
         }
-    
+
+        private void SetTemFileName(Articles article, ArticleTranslation artTranslate)
+        {
+            tempArtNr = String.Format("{0}{1}{2}{3}", _vendorID, _categoryID, _productID, _subproductID);
+            article.ArticleNumber = tempArtNr;
+            artTranslate.ArticleNumber = tempArtNr;
+        }
+
+        private void GetSubCategory(WebShopRepository context, Articles article, int SubCategoryID)
+        {
+            _subproductID = SubCategoryID;
+            _subproduct = context.SubCategories.Where(x => x.SubCategoryID == _subproductID).Select(n => n.SubCategoryName).FirstOrDefault();
+            article.SubCategoryId = _subproductID;
+        }
+
+        private void GetProduct(WebShopRepository context, Articles article, int ProductID)
+        {
+            _productID = ProductID;
+            _product = context.Products.Where(x => x.ProductID == _productID).Select(n => n.ProductName).FirstOrDefault();
+            article.ProductId = _productID;
+        }
+
+        private void GetCategory(WebShopRepository context, Articles article, int CategoryID)
+        {
+            _categoryID = CategoryID;
+            _category = context.Categories.Where(x => x.CategoryID == _categoryID).Select(n => n.CategoryName).FirstOrDefault();
+            article.CategoryId = _categoryID;
+        }
+
+        private void GetVendor(WebShopRepository context, Articles article, int VendorID)
+        {
+            _vendorID = VendorID;
+            _vendor = context.Vendors.Where(x => x.VendorID == _vendorID).Select(n => n.VendorName).FirstOrDefault();
+            article.VendorId = Convert.ToInt32(_vendorID);
+        }
+
         public async Task Image(IHostingEnvironment hostEnvironment,  WebShopRepository context, Articles article, IFormFile file, IFormCollection form, string _vendor, string _category, string _product, string _subproduct)
         {
             if (IsImage(file)==true)
@@ -103,46 +128,64 @@ namespace Webshop.Models.BusinessLayers
                 var fPath = Path.Combine(uploads, newFilename); // gets the path and filename for ifexists
                 if (File.Exists(fPath) || File.Exists(Path.Combine(Directory.GetParent(Path.GetDirectoryName(fPath)).FullName, Path.GetFileName(fPath))) == true)
                 {
-                    ext = Path.GetExtension(file.FileName); // get file extention
-                    tmpName = form["ArticleName"] + "_" + tempArtNr;
-                    tmpNameTwo = tmpName.Replace("\"", "");
-                    int countFInDir = Directory.GetFiles(uploads, "*", SearchOption.TopDirectoryOnly).Length; // count existing files in topdirectory ie. uploads
-                    int addCount = countFInDir + 1; // increment filecount 
-                    string dash = "_";
-                    string tmpNameThree = string.Format("{0}{1}{2}", tmpNameTwo, dash, addCount);
-                    string newFnameExists = tmpNameThree.Replace(" ", "_") + ext.ToString();
-                    using (var fileStream = new FileStream(Path.Combine(uploads, newFnameExists), FileMode.Create))
-                    {
-                        await file.CopyToAsync(fileStream);
-                    }
-                    ImageModel imgExists = new ImageModel
-                    {
-                        ImageDate = date,
-                        ImageName = newFnameExists,
-                        ImagePath = String.Format("{0}", serverPath),
-                        ArticleGuid = guidID
-                    };
-                    context.Images.Add(imgExists);
-                    await context.SaveChangesAsync();
+                    string newFnameExists;
+
+                    SetFileNameIfFileExists(file, form, uploads, out ext, out tmpName, out tmpNameTwo, out newFnameExists);
+
+                    await IfFileExistsUpload(context, file, serverPath, uploads, newFnameExists);
 
                 }
                 else
                 {
-                    using (var fileStream = new FileStream(Path.Combine(uploads, newFilename), FileMode.Create))
-                    {
-                        await file.CopyToAsync(fileStream);
-                    }
-                    ImageModel img = new ImageModel
-                    {
-                        ImageDate = date,
-                        ImageName = newFilename,
-                        ImagePath = String.Format("{0}", serverPath),
-                        ArticleGuid = guidID
-                    };
-                    context.Images.Add(img);
-                    await context.SaveChangesAsync();
+                    await FileUpload(context, file, serverPath, uploads, newFilename);
                 }
             }
+        }
+
+        private void SetFileNameIfFileExists(IFormFile file, IFormCollection form, string uploads, out string ext, out string tmpName, out string tmpNameTwo, out string newFnameExists)
+        {
+            ext = Path.GetExtension(file.FileName); // get file extention
+            tmpName = form["ArticleName"] + "_" + tempArtNr;
+            tmpNameTwo = tmpName.Replace("\"", "");
+            int countFInDir = Directory.GetFiles(uploads, "*", SearchOption.TopDirectoryOnly).Length; // count existing files in topdirectory ie. uploads
+            int addCount = countFInDir + 1; // increment filecount 
+            string dash = "_";
+            string tmpNameThree = string.Format("{0}{1}{2}", tmpNameTwo, dash, addCount);
+            newFnameExists = tmpNameThree.Replace(" ", "_") + ext.ToString();
+        }
+
+        private async Task IfFileExistsUpload(WebShopRepository context, IFormFile file, string serverPath, string uploads, string newFnameExists)
+        {
+            using (var fileStream = new FileStream(Path.Combine(uploads, newFnameExists), FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            ImageModel imgExists = new ImageModel
+            {
+                ImageDate = date,
+                ImageName = newFnameExists,
+                ImagePath = String.Format("{0}", serverPath),
+                ArticleGuid = guidID
+            };
+            context.Images.Add(imgExists);
+            await context.SaveChangesAsync();
+        }
+
+        private async Task FileUpload(WebShopRepository context, IFormFile file, string serverPath, string uploads, string newFilename)
+        {
+            using (var fileStream = new FileStream(Path.Combine(uploads, newFilename), FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            ImageModel img = new ImageModel
+            {
+                ImageDate = date,
+                ImageName = newFilename,
+                ImagePath = String.Format("{0}", serverPath),
+                ArticleGuid = guidID
+            };
+            context.Images.Add(img);
+            await context.SaveChangesAsync();
         }
 
         internal async Task EditArticle(Articles article, ArticleTranslation artTrans, WebShopRepository context, IHostingEnvironment hostEnvironment, int id, IFormFile file, IFormCollection form)
@@ -202,13 +245,28 @@ namespace Webshop.Models.BusinessLayers
         }
 
         private static bool IsImage(IFormFile file)
-        {         
+        {   
+            // checks if file is image
             if (file == null) return false;
             return file.ContentType.Contains("image") ||
                 _imageFileExtensions.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
         }
 
         internal async Task Translate(int id, WebShopRepository context,ArticlesViewModel artView, ArticleTranslation artTrans,string text, string one, string two, string three, string four)
+        {
+            AddTranslation(id, context, artTrans, text, one, two, three, four);
+
+            // TODO: update isTranslated for se-SV
+            var isTranslated = context.ArticleTranslations
+                .Where(x => x.ArticleId == id)
+                .Select(x => x.ISTranslated).FirstOrDefault();
+            isTranslated = true;
+            artTrans.ISTranslated = isTranslated;
+
+            await context.SaveChangesAsync();
+        }
+
+        private static void AddTranslation(int id, WebShopRepository context, ArticleTranslation artTrans, string text, string one, string two, string three, string four)
         {
             ArticleTranslation trans = new ArticleTranslation
             {
@@ -224,13 +282,6 @@ namespace Webshop.Models.BusinessLayers
                 ArticleNumber = artTrans.ArticleNumber
             };
             context.Add(trans);
-            var isTranslated = context.ArticleTranslations
-                .Where(x => x.ArticleId == id)
-                .Select(x => x.ISTranslated).FirstOrDefault();
-            isTranslated = true;
-            artTrans.ISTranslated = isTranslated;
-
-            await context.SaveChangesAsync();
         }
     }
 }
